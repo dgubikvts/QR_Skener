fd793c46928bc895ffc3a76044779e65962e36ec
dgubikvts <92539717+dgubikvts@users.noreply.github.com>
2022-03-28 14:33:02 +0200

    my-orders

--- resources/views/nav/navbar.blade.php
+++ resources/views/nav/navbar.blade.php
@@ -4,24 +4,24 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0 col-2">
-        <li class="nav-item ">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
           <a class="nav-link" aria-current="page" href="/">Pocetna</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="/scanner">Skener</a>
+          <a class="nav-link" aria-current="page" href="/scanner">Bar kod skener</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="/orders">Porudzbine</a>
+          <a class="nav-link" aria-current="page" href="/orders">Sve porudzbine</a>
         </li>
       </ul>
     </div>
-    <div class="d-flex justify-content-end align-items-center col-10">
+    <div class="d-flex justify-content-end align-items-center col-10 col-md-8">
       <a href="{{route('cart')}}" class="mx-3 korpa d-flex">
         <i class="fas fa-shopping-cart velicina mx-2"></i><p class="m-0 cartquantity" data-qty="{{ App\Http\Controllers\CartController::cartQty() }}">{{ App\Http\Controllers\CartController::cartQty() }}</p>
       </a>
       
-      <form action="{{route('search')}}" method="GET" class="d-flex col-5 col-lg-3 col-xl-2">
+      <form action="{{route('search')}}" method="GET" class="d-flex col-6 col-lg-4 col-xl-3">
         <input class="form-control me-2" type="search" placeholder="Pretrazi..." aria-label="Search" name="query" id="query">
         <button class="btn btn-outline-success" type="submit" id="pretraga"><i class="fal fa-search"></i></button>
       </form>
--- resources/views/pages/my-orders.blade.php
+++ resources/views/pages/my-orders.blade.php
@@ -1,17 +1,44 @@
 @extends('layouts/app')
 @section('content')
 
-@forelse(Auth::user()->orders as $order)
-    @foreach($order->order_item as $item)
-        <img src="{{url($item->product->slika)}}" alt="Image" class="img-fluid col-2"/>
-        {{$item->product->naziv}}
-        {{$item->product->cena}}
-        x{{$item->quantity}}
-        <br>
-    @endforeach
-    <hr>
-@empty
-    <p>No orders</p>
-@endforelse
+<h3 class="text-center">Moje porudzbine</h3>
+<div class="row row-cols-1 g-4 col-12 col-md-8 m-auto">
+    @forelse(Auth::user()->orders as $order)
+    <div class="col">
+        <div class="card">
+            <h5 class="card-title text-center mt-3">Porudzbina #{{$order->id}}</h5>
+            <hr>
+            <table class="table text-center table-borderless">
+                <tbody>
+                    @foreach($order->order_item as $item)
+                    <tr class="align-middle">
+                        <td class="p-0 m-0"><img src="{{url($item->product->slika)}}" alt="Image" class="korpa-img p-0 m-0"/></td>
+                        <td>{{$item->product->naziv}}</td>
+                        <td>{{$item->product->cena}}rsd</td>
+                        <td>x{{$item->quantity}}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            <hr>
+            <p class="lead text-end me-2 total" data-cena="{{$order->cena}}"></p>
+        </div>
+    </div>
+        
+    @empty
+        <p>No orders</p>
+    @endforelse
+ 
+  
+</div>
+
+<script>
+    $(document).ready(function () {
+        $(".total").each(function() {
+            $(this).html("Ukupno: " + formatRSD($(this).attr('data-cena')) + 'rsd');
+        });
+    });
+    
+</script>
 
 @endsection
